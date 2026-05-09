5f8f854a9953f573fae0c63bbde08a6c614b1bc2
Ismoil-21 <toxirovi82@gmail.com>
2026-05-09 19:08:31 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+      <div>
+        Ismoil
+      </div>
+      <div>
+        Mirzohid
+      </div>
+      <div>
+        Toxr
+      </div>
     </div>
   )
 }
